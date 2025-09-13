ec44d7c0b9f9bc11c6ef2a8313d9004537527ba1
9bingyin <realbingyin@outlook.com>
2025-09-14 02:17:41 +0800

    优化 longbridge 包依赖，移除不必要的库并整理结构

--- pkgs/longbridge/default.nix
+++ pkgs/longbridge/default.nix
@@ -4,49 +4,27 @@
 , dpkg
 , autoPatchelfHook
 , makeWrapper
-, wayland
-, libxkbcommon
-, xorg
-, libGL
-, libglvnd
-, vulkan-loader
-, mesa
-, libdrm
-, gtk3
-, gtk4
+# 核心 GTK/GUI 依赖
 , glib
+, gtk3
 , cairo
 , pango
 , gdk-pixbuf
-, atk
-, fontconfig
 , freetype
-, alsa-lib
-, libpulseaudio
-, pipewire
-, openssl
-, curl
-, zlib
-, zstd
-, gsettings-desktop-schemas
-, libnotify
-, libsecret
-, at-spi2-core
-, at-spi2-atk
-, sqlite
-, libgit2
+# Web 引擎
 , webkitgtk_4_1
 , libsoup_3
-, cups
-, systemd
-, bzip2
+# X11/键盘支持
+, xorg
+, libxkbcommon
+# 音频
+, alsa-lib
+# 数据库
+, sqlite
+# 加密
+, openssl
+# 压缩
 , xz
-, expat
-, nspr
-, nss
-, libgudev
-, librsvg
-, util-linux
 }:
 
 stdenv.mkDerivation rec {
@@ -65,56 +43,36 @@ stdenv.mkDerivation rec {
   ];
 
   buildInputs = [
-    wayland
-    libxkbcommon
-    xorg.libX11
-    xorg.libXext
-    xorg.libxcb
-    xorg.libXcomposite
-    xorg.libXdamage
-    xorg.libXfixes
-    xorg.libXrandr
-    xorg.libXScrnSaver
-    libGL
-    libglvnd
-    vulkan-loader
-    mesa
-    libdrm
-    gtk3
-    gtk4
+    # GTK/GUI 核心依赖
     glib
+    gtk3
     cairo
     pango
     gdk-pixbuf
-    atk
-    fontconfig
     freetype
-    alsa-lib
-    libpulseaudio
-    pipewire
-    openssl
-    curl
-    zlib
-    zstd
-    gsettings-desktop-schemas
-    libnotify
-    libsecret
-    at-spi2-core
-    at-spi2-atk
-    sqlite
-    libgit2
+
+    # Web 引擎 (基于 WebKitGTK)
     webkitgtk_4_1
     libsoup_3
-    cups
-    systemd
-    bzip2
+
+    # X11 支持
+    xorg.libX11
+    xorg.libxcb
+    libxkbcommon
+
+    # 音频支持
+    alsa-lib
+
+    # 数据库
+    sqlite
+
+    # 加密库
+    openssl
+
+    # 压缩支持
     xz
-    expat
-    nspr
-    nss
-    libgudev
-    librsvg
-    util-linux
+
+    # C++ 标准库
     stdenv.cc.cc.lib
   ];
 
@@ -125,15 +83,7 @@ stdenv.mkDerivation rec {
 
     install -Dm755 usr/local/bin/longbridge $out/bin/.longbridge-unwrapped
 
-    makeWrapper $out/bin/.longbridge-unwrapped $out/bin/longbridge \
-      --prefix LD_LIBRARY_PATH : "${
-        lib.makeLibraryPath [
-          vulkan-loader
-          libGL
-          libglvnd
-          mesa
-        ]
-      }"
+    makeWrapper $out/bin/.longbridge-unwrapped $out/bin/longbridge
 
     install -Dm644 usr/share/icons/hicolor/512x512/apps/longbridge.png \
       $out/share/icons/hicolor/512x512/apps/longbridge.png
